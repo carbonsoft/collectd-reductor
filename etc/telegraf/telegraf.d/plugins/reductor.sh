507de9f3e99d84f4afd52de907072e638e6c232c
Oleg Strizhechenko <oleg.strizhechenko@gmail.com>
2016-12-18 12:06:34 +0500

    Исправлено(плагин к телеграф): удалены старые плагины, в общий плагин добавлен сбор данных по всем модулям, размеров кэша и списков
    CR-449

--- etc/telegraf/telegraf.d/plugins/reductor.sh
+++ etc/telegraf/telegraf.d/plugins/reductor.sh
@@ -2,8 +2,35 @@
 
 set -eu
 
-for module in ipt_reductor ipt_dnsmatch ipt_snimatch; do
-        sed -E "s/^/$module./; s/ +/=/g" /proc/net/$module/block_list
-done
-echo "ipt_reductor.signatures.cache=$(wc -l </app/reductor/var/lib/reductor/cache/signatures.cache)"
-find /app/reductor/var/lib/reductor/lists/{rkn,load}/ -type f -exec wc -l {} \; | awk '{print $2"="$1}' | sed 's|.*/||; s|^|lists.|'
+read_module() {
+	local module="$1"
+	sed -E "s/^/$module./; s/ +/=/g" /proc/net/$module/block_list
+}
+
+modules() {
+	read_module ipt_reductor
+
+	if chroot /app/reductor/ /usr/local/Reductor/bin/dnsenabled; then
+		read_module ipt_dnsmatch
+	fi
+
+	if chroot /app/reductor/ /usr/local/Reductor/bin/dnsenabled --sni; then
+		read_module ipt_snimatch
+	fi
+}
+
+signatures_cache() {
+	echo "ipt_reductor.signatures.cache=$(wc -l </app/reductor/var/lib/reductor/cache/signatures.cache)"
+}
+
+lists_sizes() {
+	find /app/reductor/var/lib/reductor/lists/{rkn,load}/ -type f -exec wc -l {} \; | awk '{print $2"="$1}' | sed 's|.*/||; s|^|lists.|'
+}
+
+main() {
+	modules
+	signatures_cache
+	lists_sizes
+}
+
+main "$@"
